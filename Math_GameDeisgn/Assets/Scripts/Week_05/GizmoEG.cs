555eb754db40c2b50099176ec8b107d6b4fa5fd5
Unknown <gordon.lee.6170@gmail.com>
2018-02-21 15:21:26 -0500

    week 5 in class stuff

---
+++ Math_GameDeisgn/Assets/Scripts/Week_05/GizmoEG.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GizmoEG : MonoBehaviour {
+
+    public Transform bodyA, bodyB;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, bodyA.position);
+
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawLine(transform.position, bodyB.position);
+
+        Gizmos.color = Color.magenta;
+        float dotProd = Vector3.Dot(bodyA.position, bodyB.position);
+        Gizmos.DrawRay(transform.position, Vector3.up * dotProd);
+
+    }
+
+    private void OnDrawGizmosSelected() {
+        
+    }
+}
