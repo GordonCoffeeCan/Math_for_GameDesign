f18a8c486bb94f89599513c6d5e1342a3b2da473
Unknown <gordon.lee.6170@gmail.com>
2018-02-28 12:09:00 -0500

    in class week 6

---
+++ Math_GameDeisgn/Assets/Scripts/Week_06/SpinTranslate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinTranslate : MonoBehaviour {
+
+    public Vector3 angularMomentum;
+
+    private Matrix4x4 inertiaMatrix;
+
+	// Use this for initialization
+	void Start () {
+        inertiaMatrix = Matrix4x4.Scale(GetComponent<Rigidbody>().inertiaTensor);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CalcRotation();
+    }
+
+    private void CalcRotation() {
+        Matrix4x4 _rotationMatrix = Matrix4x4.TRS(Vector3.zero, transform.rotation, Vector3.one);
+        Matrix4x4 _inertialMatrixLocal = _rotationMatrix * inertiaMatrix * _rotationMatrix.inverse;
+        Vector3 _angularVelocity = _inertialMatrixLocal.inverse * angularMomentum;
+        Vector3 _axis = _angularVelocity.normalized;
+        float _speed = _angularVelocity.magnitude;
+        float _degreesThisFrame = Time.deltaTime * Mathf.Rad2Deg;
+        transform.RotateAround(transform.position, _axis, _speed * _degreesThisFrame);
+    }
+}
---
+++ Math_GameDeisgn/Assets/Scripts/Week_06/TransformEdit.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformEdit : MonoBehaviour {
+
+    public Vector3 translation;
+    public Vector3 eulerAngles;
+    public Vector3 scale = Vector3.one;
+
+    private MeshFilter myMesh;
+    private Vector3[] originalVert;
+    private Vector3[] newVerts;
+
+    // Use this for initialization
+    void Start () {
+        myMesh = GetComponent<MeshFilter>();
+        originalVert = myMesh.mesh.vertices;
+        newVerts = new Vector3[originalVert.Length];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Quaternion _rotation = Quaternion.Euler(eulerAngles.x, eulerAngles.y, eulerAngles.z);
+        Matrix4x4 _m = Matrix4x4.identity;
+        _m.SetTRS(translation, _rotation, scale);
+
+        for (int i = 0; i < originalVert.Length; i++) {
+            newVerts[i] = _m.MultiplyPoint3x4(originalVert[i]);
+        }
+
+        myMesh.mesh.vertices = newVerts;
+	}
+}
