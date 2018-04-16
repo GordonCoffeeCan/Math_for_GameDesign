9d9f79393b794dda39aacac44030e7efcdc86033
Unknown <gordon.lee.6170@gmail.com>
2018-04-16 15:01:44 -0400

    assignement done!

---
+++ Math_GameDeisgn/Assets/Scripts/Week_11/PlatformRandomRotate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformRandomRotate : MonoBehaviour {
+    private float valueTimer = 0;
+    private Vector3 randomRotation = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+        valueTimer = Random.Range(1f, 2f);
+        randomRotation = new Vector3(Random.value * Random.Range(-45f, 45f), 0, Random.value * Random.Range(-45f, 45f));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        valueTimer -= Time.deltaTime;
+        if (valueTimer <= 0) {
+            valueTimer = Random.Range(1f, 2f);
+            randomRotation = new Vector3(Random.value * Random.Range(-30f, 30f), 0, Random.value * Random.Range(-30f, 30f));
+        }
+
+        this.transform.rotation = Quaternion.Lerp(this.transform.rotation, Quaternion.Euler(randomRotation), 0.05f);
+	}
+}
